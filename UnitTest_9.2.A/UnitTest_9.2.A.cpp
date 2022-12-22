fdc60bbbdbcedfacccf1f6660b5a36d897e98485
viksi01 <victoria.pyatnicka@gmail.com>
2022-12-22 21:57:15 +0200

    commit_9.2

---
+++ UnitTest_9.2.A/UnitTest_9.2.A.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_9.2.A/lab_9.2.A.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest91A
+{
+	TEST_CLASS(UnitTest91A)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Student* p = new Student;
+			p->prizv = "A";
+			p->kurs = 2;
+			p->spec = KN;
+
+			int t = BinSearch(p, 1, "A", 2, KN  );
+
+			Assert::AreEqual(t, 0);
+
+		}
+	};
+}
